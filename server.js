080e8f8f0c4c31b72ce390f1327a0e84753b655d
deLeonRodrigo <rodri230998@gmail.com>
2019-10-07 10:45:35 -0500

    modularized

--- server.js
+++ server.js
@@ -1,43 +1,5 @@
-const {
-	ApolloServer,
-	gql
-} = require('apollo-server')
 const Mongoose = require("mongoose")
-const Schema = Mongoose.Schema
-const dotenv = require('dotenv')
-const typeDefs = gql `
-	type Articulo {
-		id: ID
-		nombre: String
-		codigoDeBarras: String
-		codigo: String
-	}
-	type EntradaInventario {
-		id: ID
-		idArticulo: Articulo
-		idInventario: Inventario
-		cantidad: Float
-	}
-	type Almacen {
-		id: ID
-		nombre: String
-	}
-	type Inventario {
-		id: ID
-		nombre: String
-		almacen: Almacen
-	}
-	type Query {
-		articulo(codigoDeBarras: String): Articulo
-		entradaInventario(inventario: String): [EntradaInventario]
-		inventario(inventario: String) : Inventario
-	}
-	type Mutation {
-		addEntradaInventario(idArticulo: String, idInventario: String, cantidad: Float): EntradaInventario
-		addAlmacen(nombre: String): Almacen
-		addInventario(idAlmacen: String, nombre: String): Inventario
-	}
-`
+const dotenv = require("dotenv")
 
 dotenv.config()
 
@@ -46,101 +8,21 @@ Mongoose.connect(process.env.DB, {
 	useUnifiedTopology: true
 })
 
-const ArticuloModel = Mongoose.model("Articulo", {
-	nombre: String,
-	codigoDeBarras: String,
-	codigo: String
-}, "articulos")
-
-const InventarioModel = Mongoose.model("Inventario", {
-	nombre: String,
-	almacen: {
-		type: Mongoose.Schema.Types.ObjectId,
-		ref: 'Almacen',
-		resolver: async (parent, args, {}) =>
-			await AlmacenModel.findOne({
-				_id: parent.almacen
-			}),
-	},
-}, "inventarios")
-
-const AlmacenModel = Mongoose.model("Almacen", {
-	nombre: String
-}, "almacens")
-
-const EntradaInventarioModel = Mongoose.model("EntradaInventario", {
-	idArticulo: {
-		type: Mongoose.Schema.Types.ObjectId,
-		ref: 'Articulo',
-		resolver: async (parent, args, {}) =>
-			await ArticuloModel.findOne({
-				_id: parent.idArticulo
-			})
-	},
-	idInventario: {
-		type: Mongoose.Schema.Types.ObjectId,
-		ref: 'Inventario',
-		resolver: async (parent, args, {}) =>
-			await InventarioModel.findOne({
-				_id: parent.idInventario
-			})
-			.populate('almacen')
-			.exec(),
-	},
-	cantidad: Number
-}, "entradainventarios")
-
-const resolvers = {
-	Query: {
-		articulo: async (_, args) => await ArticuloModel.findOne({
-			'codigoDeBarras': args.codigoDeBarras
-		}).exec(),
-		entradaInventario: async (_, args) =>
-			await EntradaInventarioModel.find({
-				idInventario: args.inventario
-			})
-			.populate([{
-				model: 'Inventario',
-				path: 'idInventario',
-				populate: {
-					model: 'Almacen',
-					path: 'almacen'
-				}
-			},
-			{
-				model: 'Articulo',
-				path: 'idArticulo'
-			}
-		])
-			.exec(),
-		inventario: async (_, args) =>
-			await InventarioModel.findOne({_id: args.inventario})
-			.populate('almacen')
-			.exec(),
-	},
-	Mutation: {
-		addEntradaInventario: async (_, args) => {
-			var entrada = new EntradaInventarioModel(args)
-			return await entrada.save();
-		},
-		addAlmacen: async (_, args) => {
-			var almacen = new AlmacenModel(args)
-			return await almacen.save();
-		},
-		addInventario: async (_, args) => {
-			var inventario = new InventarioModel(args)
-			return await inventario.save();
-		},
-	},
-}
+const {
+	ApolloServer
+} = require('apollo-server')
+const typeDefs = require('./schema').typeDefs
+const resolvers = require('./graphql').resolvers
 
 const server = new ApolloServer({
 	typeDefs,
 	resolvers,
 	introspection: true,
 	playground: true
-})
+});
 
-server.listen(process.env.PORT || 5000).then(({url}) => {
-	console.log(`🚀  Server ready at ${url}`);
-})
+server.listen(process.env.PORT || 4000).then(({
+	url
+}) => {
+	console.log(`🚀 Server ready at ${url}`);
+});

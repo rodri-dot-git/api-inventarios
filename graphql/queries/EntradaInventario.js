5b0927d9b8e7b8b6e4d3b2f8c7da95874bca4e08
deLeonRodrigo <rodri230998@gmail.com>
2019-10-10 23:07:49 -0500

    no repeat

--- graphql/queries/EntradaInventario.js
+++ graphql/queries/EntradaInventario.js
@@ -1,10 +1,25 @@
 const EntradaInventarioModel = require('../../models').EntradaInventario
 
+function getUnique(arr, comp) {
+
+    const unique = arr
+        .map(e => e[comp])
+
+        // store the keys of the unique objects
+        .map((e, i, final) => final.indexOf(e) === i && i)
+
+        // eliminate the dead keys & store unique objects
+        .filter(e => arr[e]).map(e => arr[e]);
+
+    return unique;
+}
+
+
 module.exports = {
     EntradaInventario: {
         Query: {
-            entradaInventario: async (_, args) =>
-                await EntradaInventarioModel.find({
+            entradaInventario: async (_, args) =>{
+                var datos = await EntradaInventarioModel.find({
                     idInventario: args.inventario
                 })
                 .populate([{
@@ -20,7 +35,16 @@ module.exports = {
                         path: 'idArticulo'
                     }
                 ])
-                .exec(),
+                .exec()
+                console.log(datos);
+                datos.forEach(x => {
+                    for (let i = 0; i < datos.length; i++) {
+                        if(x.idArticulo == datos[i].idArticulo)
+                            x.cantidad += datos[i].cantidad
+                    }
+                })
+                return getUnique(datos, "idArticulo");
+            }
         }
     }
 }

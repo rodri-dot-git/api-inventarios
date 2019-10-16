e5343fe1612c33afa4ad2c7097ece904d70b9e69
deLeonRodrigo <rodri230998@gmail.com>
2019-10-16 10:32:55 -0500

    log mutation

--- graphql/mutations/Usuario.js
+++ graphql/mutations/Usuario.js
@@ -1,11 +1,17 @@
 const UsuarioModel = require('../../models').Usuario
+const rollbar = require('../../utils').rollbar
 
 module.exports = {
     Usuario: {
         Mutation: {
             addUsuario: async (_, args) => {
-                var usuario = new UsuarioModel(args)
-                return await usuario.save();
+                try {
+                    var usuario = await new UsuarioModel(args).save()
+                    rollbar.log('Usuario agregado correctamente')
+                    return await usuario;
+                } catch (error) {
+                    rollbar.log(`Error en user fetch. error: ${error}`)
+                }
             },
         }
     }
